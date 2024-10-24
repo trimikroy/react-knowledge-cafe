8aae3f89c70d609001be811425f4d91ba831692f
trimikpro <vupotiroy112@gmail.com>
2024-10-24 08:17:30 +0600

    react-knowledge-cafe

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,11 @@
+
+
+const Bookmarks = () => {
+    return (
+        <div className="1/3">
+            <h2 className="text-4xl">Book Marks </h2>
+        </div>
+    );
+};
+
+export default Bookmarks;
